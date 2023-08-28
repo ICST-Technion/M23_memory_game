70a4002cd5ed2fe214f58d9ba5fa841181fb7703
OrBroder <64217068+OrBroder@users.noreply.github.com>
2023-08-28 14:26:37 +0300

    Update Classic_BL_Com.ino

--- Classic_BL_Com.ino
+++ Classic_BL_Com.ino
@@ -1,9 +1,3 @@
-//This example code is in the Public Domain (or CC0 licensed, at your option.)
-//By Evandro Copercini - 2018
-//
-//This example creates a bridge between Serial and Classical Bluetooth (SPP)
-//and also demonstrate that SerialBT have the same functionalities of a normal Serial
-
 #include "BluetoothSerial.h"
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
@@ -12,18 +6,43 @@
 
 BluetoothSerial SerialBT;
 
+const int ledPin = 2;  // This is usually the internal LED pin for ESP32. Adjust if necessary.
+bool ledState = false; // To track the current state of the LED.
+String readBuffer = ""; // Buffer to store incoming characters to check for the "light_led" string.
+
 void setup() {
   Serial.begin(115200);
   SerialBT.begin("ESP32test"); //Bluetooth device name
+  pinMode(ledPin, OUTPUT); // Set the LED pin as output
   Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
 void loop() {
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
+  // Read and forward data from wired Serial to Bluetooth Serial
+  while (Serial.available()) {
+    char c = Serial.read();
+    SerialBT.write(c);  
+    readBuffer += c;
+    checkCommand();
   }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
+
+  // Read and forward data from Bluetooth Serial to wired Serial
+  while (SerialBT.available()) {
+    char c = SerialBT.read();
+    Serial.write(c);  
+    readBuffer += c;
+    checkCommand();
   }
-  delay(20);
+}
+
+void checkCommand() {
+  if (readBuffer.length() > 100) {  // Trim the buffer if it gets too long.
+    readBuffer = readBuffer.substring(readBuffer.length() - 100);
+  }
+
+  if (readBuffer.indexOf("light_led") != -1) {  // If "light_led" is found in the buffer...
+    ledState = !ledState;  // Toggle LED state
+    digitalWrite(ledPin, ledState ? HIGH : LOW);  // Turn LED on/off based on the new state
+    readBuffer = "";  // Clear the buffer to avoid processing the same command multiple times.
+  }
 }
